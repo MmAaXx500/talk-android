ef27b706b385972366b5e9f51a64cb1fa5e7f73b
MmAaXx500 <viktor.balogh45@gmail.com>
2024-12-04 22:09:20 +0100

    Revert "remove comment"
    This reverts commit be99e1a037847a9dc2b490ef5ad3c498e48bdd37.

--- app/src/main/java/com/nextcloud/talk/jobs/NotificationWorker.kt
+++ app/src/main/java/com/nextcloud/talk/jobs/NotificationWorker.kt
@@ -836,6 +836,8 @@ class NotificationWorker(context: Context, workerParams: WorkerParameters) : Wor
         }
         notificationManager.notify(notificationId, notification)
 
+        // On devices with Android 8.0 (Oreo) or later, notification sound will be handled by the system
+        // if notifications have not been disabled by the user.
         return
     }
 
